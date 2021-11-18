0694a15d92d0b9e7d1b0a875973048af049302de
Mausp7 <archie.tombacz@gmail.com>
2021-11-18 10:44:27 +0100

    Homework css  and js added.

--- script.js
+++ script.js
@@ -1,6 +1,50 @@
+const generateArray = function (amount) {
+    let returnArray = [];
+    amount = parseInt(amount);
+
+    if (!Number.isNaN(amount)) {
+        for (let i = 0; i < amount; i++) {
+            returnArray.push(`${numFormatter(i, amount)}`);
+        };
+    } else {
+        returnArray.push(`error`);
+
+    };
+
+    return returnArray;
+};
+
+function numFormatter(num, max) {
+    let zeroBefore = "";
+
+    for (let i = 0; i < max.toString().length - num.toString().length; i++) {
+        zeroBefore += "0";      
+    };
+
+    return zeroBefore += num;
+};
+
 function loadEvent() {
-    const rootElement = document.getElementById("root");
-    console.log("Loaded");
-}
+    const root = document.getElementById("root");
+    const list = generateArray(10);
+
+    if (list[0] !== "error") {
+        for (const item of list) {
+            root.insertAdjacentHTML("beforeend", `<div>${item}</div>`)
+        }        
+    } else {
+        
+    };
+};
 
 window.addEventListener("load", loadEvent);
+
+/* 
+window.addEventListener("load", function(){
+    Ez is egy valid megoldás.
+});
+
+window.addEventListener("load", _ => {
+    Ez is egy valid megoldás.
+});
+*/
